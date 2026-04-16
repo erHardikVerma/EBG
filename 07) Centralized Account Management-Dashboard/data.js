c6a8dcca6fb4472a94479e08d7f9dca76826985b
Hardik Verma <hardik.ebg@example.com>
2026-04-16 15:14:42 +0530

    Add 07) Centralized Account Management-Dashboard folder

---
+++ 07) Centralized Account Management-Dashboard/data.js
@@ -0,0 +1,83 @@
+const dashboardData = [
+  {
+    "DisplayName": "Employee 1",
+    "UPN": "employee1@companya.com",
+    "Company": "Company A",
+    "Department": "Department A",
+    "Region": "Region A",
+    "CostCenter": "1001",
+    "Licenses": "License A"
+  },
+  {
+    "DisplayName": "Employee 2",
+    "UPN": "employee2@companya.com",
+    "Company": "Company A",
+    "Department": "Department B",
+    "Region": "Region B",
+    "CostCenter": "1002",
+    "Licenses": "License B"
+  },
+  {
+    "DisplayName": "Employee 3",
+    "UPN": "employee3@companya.com",
+    "Company": "Company A",
+    "Department": "Department C",
+    "Region": "Region C",
+    "CostCenter": "1003",
+    "Licenses": "License C"
+  },
+  {
+    "DisplayName": "Employee 1",
+    "UPN": "employee1@companyb.com",
+    "Company": "Company B",
+    "Department": "Department A",
+    "Region": "Region A",
+    "CostCenter": "2001",
+    "Licenses": "License A"
+  },
+  {
+    "DisplayName": "Employee 2",
+    "UPN": "employee2@companyb.com",
+    "Company": "Company B",
+    "Department": "Department B",
+    "Region": "Region B",
+    "CostCenter": "2002",
+    "Licenses": "License B"
+  },
+  {
+    "DisplayName": "Employee 3",
+    "UPN": "employee3@companyb.com",
+    "Company": "Company B",
+    "Department": "Department C",
+    "Region": "Region C",
+    "CostCenter": "2003",
+    "Licenses": "License C"
+  },
+  {
+    "DisplayName": "Employee 1",
+    "UPN": "employee1@companyc.com",
+    "Company": "Company C",
+    "Department": "Department A",
+    "Region": "Region A",
+    "CostCenter": "3001",
+    "Licenses": "License A"
+  },
+  {
+    "DisplayName": "Employee 2",
+    "UPN": "employee2@companyc.com",
+    "Company": "Company C",
+    "Department": "Department B",
+    "Region": "Region B",
+    "CostCenter": "3002",
+    "Licenses": "License B"
+  },
+  {
+    "DisplayName": "Employee 3",
+    "UPN": "employee3@companyc.com",
+    "Company": "Company C",
+    "Department": "Department C",
+    "Region": "Region C",
+    "CostCenter": "3003",
+    "Licenses": "License C"
+  }
+];
